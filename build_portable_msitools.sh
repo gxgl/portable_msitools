b2a3e19fcb78b180a0e9e4cb7c6625053313743f
George Stamate <stamate_george@yahoo.com>
2023-04-12 23:02:01 +0300

    Added tests.yml

--- build_portable_msitools.sh
+++ build_portable_msitools.sh
@@ -165,7 +165,7 @@ do
             # File exists, run the code
             echo ""
             echo "appimage-builder --recipe $recipe"
-            appimage-builder --recipe $recipe
+            sudo appimage-builder --recipe $recipe
             echo ""
         else
             echo "No $filename"
@@ -188,7 +188,7 @@ do
             echo "Rebuilding $filename"
             echo ""
             echo "appimage-builder --recipe $recipe"
-            appimage-builder --recipe $recipe
+            sudo appimage-builder --recipe $recipe
             echo ""
         fi
     fi
